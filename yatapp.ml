c4ee47aece60612b31352425b3290ea4e13d2fd0
Kevin <ksrof@proton.me>
2023-06-14 08:35:19 +0200

    wip: working on a function to read file content to string

--- yatapp.ml
+++ yatapp.ml
@@ -14,5 +14,18 @@ let write_to_file (file: string) (text: string) : unit =
       let oc = open_out file in (* create or truncate file, return channel *)
         Printf.fprintf oc "%s\n" text; (* write something *)
         close_out oc;; (* flush and close the channel *)
-    
-write_to_file file "{}";;
+
+(* Read a file and return its content *)
+let read_file_to_string (file: string) : string =
+  if String.length(file) <= 0
+    then
+      raise (Invalid_argument "invalid {file} argument provided")
+    else
+      let ch = open_in_bin file in
+      let contents = really_input_string ch (in_channel_length ch) in
+      close_in ch;
+      contents;;
+
+write_to_file file "{\n}";;
+let contents = read_file_to_string file;;
+Printf.printf "File content: \n%s\n" contents;;
